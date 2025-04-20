d2c56a6bf823a738b1fbc9c3fe1eaf010eb92e17
David Sisco <dsisco11@gmail.com>
2025-04-20 01:44:53 +0000

    [chore]: refactor mask-mode into proper enum

--- primitives.py
+++ primitives.py
@@ -1,25 +1,27 @@
-from typing import Literal
+from enum import Enum
 
 from pydantic import BaseModel, Field
 
 from invokeai.app.invocations.baseinvocation import BaseInvocationOutput, invocation_output
 from invokeai.app.invocations.fields import ImageField, OutputField
 
-MaskingMode = Literal[
-    "boolean",# eg: boolean tensor, shape: [1, H, W], dtype: bool
-    "scalar",# eg: scalar(0.0 - 1.0) tensor, shape: [1, H, W], dtype: float8/16/32
-    "uint8", # eg: uint8 tensor, shape: [1, H, W], dtype: uint8
-    "uint16",# eg: uint16 tensor, shape: [1, H, W], dtype: uint16
-    "image_luminance", # eg: image mode='L', dtype: uint8
-    "image_alpha", # eg: image mode='RGBA', dtype: uint8, alpha channel is the mask
-    "image_compound",# eg: each channel is a different mask
-]
+
+class EMaskMode(str, Enum):
+    """Enum for Masking Modes, which determine the form of a given image mask."""
+
+    BOOLEAN = "boolean" # eg: boolean tensor, shape: [1, H, W], dtype: bool
+    SCALAR = "scalar" # eg: scalar(0.0 - 1.0) tensor, shape: [1, H, W], dtype: float8/16/32
+    UINT8 = "uint8" # eg: uint8 tensor, shape: [1, H, W], dtype: uint8
+    UINT16 = "uint16"# eg: uint16 tensor, shape: [1, H, W], dtype: uint16
+    IMAGE_LUMINANCE = "image_luminance" # eg: image mode='L', dtype: uint8
+    IMAGE_ALPHA = "image_alpha" # eg: image mode='RGBA', dtype: uint8, alpha channel is the mask
+    IMAGE_COMPOUND = "image_compound"# eg: each channel is a different mask
 
 class MaskField(BaseModel):
     """A masking primitive field."""
 
     asset_id: str = Field(description="The id/name of the mask image within the asset cache system.")
-    mode: MaskingMode = Field(description="The masking mode specifies how the mask is represented.")
+    mode: EMaskMode = Field(description="The masking mode specifies how the mask is represented.")
 
 
 @invocation_output("adv_mask_output")
