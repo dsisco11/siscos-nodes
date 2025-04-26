fix: tensor normalization methods

src/siscos_nodes/util/tensor_common.py
@@ -105,23 +105,61 @@ def apply_feathering_ellipse(
         return out[0]
     return out
 
-# @torch.jit.script
+@torch.no_grad()
+@torch.jit.script
 def normalize_tensor(tensor: torch.Tensor) -> torch.Tensor:
     """Normalize each H/W layer(separately) of the tensor to the range [0, 1]."""
     ih = tensor.dim() - 2
     iw = ih + 1
     vmin = tensor.amin(dim=(ih, iw), keepdim=True)
     vmax = tensor.amax(dim=(ih, iw), keepdim=True)
-    # avoid division by zero if a slice is constant
-    range = vmax - vmin
-    range[range == 0] = 1e-6
-    # return ((tensor - vmin) / range)
-    return torch.where(
-        range != 0,
-        (tensor - vmin) / range,
-        torch.ones_like(tensor)
-    )
 
+    # if a layer is zero-filled, we want to ensure the normalized result is also zero.
+    # logically, the layer is zeroed only if the min and max are both equal to zero
+    # boolean logic deduction shows that this is equivalent to: !(vmin != 0 | vmax != 0)
+    # so we use (vmin != 0 | vmax != 0) to get a boolean tensor of the same shape as vmin/vmax, and then later multiply the tensor by it to zero out the null layers.
+    layer_is_non_zero = torch.logical_or(vmin, vmax)
+
+    # avoid division by zero for layers filled with a single constant value by adding a small epsilon to the range
+    eps = 1e-06 # torch.finfo(tensor.dtype).eps # e.g. ~1.19e-07 for float32
+    range = (vmax - vmin) + eps
+
+    # add epsilon to cause layers with a constant value to result in 1.0 after normalization
+    result = (tensor - vmin) + eps 
+    # zero out the null layers
+    result *= layer_is_non_zero
+    # normalize each [H,W] slice to [0,1]
+    return result / range
+
+@torch.no_grad()
+@torch.jit.script
+def threshold_and_normalize_tensor(tensor: torch.Tensor, threshold: float) -> torch.Tensor:
+    """Normalize each H/W layer(separately) of the tensor to the range [0, 1], then offset by the threshold and renormalize."""
+    ih = tensor.dim() - 2
+    iw = ih + 1
+    vmin = tensor.amin(dim=(ih, iw), keepdim=True)
+    vmax = tensor.amax(dim=(ih, iw), keepdim=True)
+
+    # if a layer is zero-filled, we want to ensure the normalized result is also zero.
+    # logically, the layer is zeroed only if the min and max are both equal to zero
+    # boolean logic deduction shows that this is equivalent to: !(vmin != 0 | vmax != 0)
+    # so we use (vmin != 0 | vmax != 0) to get a boolean tensor of the same shape as vmin/vmax, and then later multiply the tensor by it to zero out the null layers.
+    layer_is_non_zero = torch.logical_or(vmin, vmax)
+
+    # avoid division by zero for layers filled with a single constant value by adding a small epsilon to the range
+    eps = 1e-06 # torch.finfo(tensor.dtype).eps # e.g. ~1.19e-07 for float32
+    range = (vmax - vmin) + eps
+
+    # add epsilon to cause layers with a constant value to result in 1.0 after normalization
+    result = (tensor - vmin) + eps 
+    # zero out the null layers
+    result *= layer_is_non_zero
+    # normalize each [H,W] slice to [0,1]
+    result /= range
+    inv = 1.0 / ((1.0 - threshold) + eps)
+    return (result - threshold).mul(inv).clamp(min=0.0, max=1.0)
+
+@torch.no_grad()
 @torch.jit.script
 def normalize_logits(logits: torch.Tensor) -> torch.Tensor:
     """Normalize each H/W layer(separately) of the logits to the range [-1, 1]"""

tests/util/test_tensor_common.py
@@ -1,9 +1,36 @@
+from typing import Final
+
 import pytest
 import torch
-from src.siscos_nodes.util.tensor_common import (
+
+from siscos_nodes.src.siscos_nodes.util.tensor_common import (
     normalize_logits,
     normalize_tensor,
+    threshold_and_normalize_tensor,
 )
+
+# region: constants
+# 1x2x2 tensors
+T22_INC_NORM: Final = torch.tensor([[0.0, 0.3333], [0.6667, 1.0]])
+T22_INC_NORM_2: Final = torch.tensor([[0.0, 0.0], [0.3333, 1.0]])
+T22_INC_NORM_NEG: Final = torch.tensor([[-1.0, -0.3333], [0.3333, 1.0]])
+T22_FILL_0: Final = torch.tensor([[0.0, 0.0], [0.0, 0.0]])
+T22_FILL_1: Final = torch.tensor([[1.0, 1.0], [1.0, 1.0]])
+T22_FILL_2: Final = torch.tensor([[2.0, 2.0], [2.0, 2.0]])
+T22_INC_0: Final = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+T22_INC_1: Final = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
+T22_INC_5: Final = torch.tensor([[5.0, 6.0], [7.0, 8.0]])
+
+# 4x2x2 tensors
+T422_INC_NORM: Final = torch.tensor([[[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]]])
+T422_INC_NORM_2: Final = torch.tensor([[[0.0, 0.0], [0.3333, 1.0]], [[0.0, 0.0], [0.3333, 1.0]], [[0.0, 0.0], [0.3333, 1.0]], [[0.0, 0.0], [0.3333, 1.0]]])
+T422_INC_NORM_NEG: Final = torch.tensor([[[-1.0, -0.3333], [0.3333, 1.0]], [[-1.0, -0.3333], [0.3333, 1.0]], [[-1.0, -0.3333], [0.3333, 1.0]], [[-1.0, -0.3333], [0.3333, 1.0]]])
+T422_FILL_0: Final = torch.fill(torch.empty(4, 2, 2), 0.0)
+T422_FILL_1: Final = torch.fill(torch.empty(4, 2, 2), 1.0)
+T422_FILL_2: Final = torch.fill(torch.empty(4, 2, 2), 2.0)
+T422_INC_0: Final = torch.tensor([[[0.0, 1.0], [2.0, 3.0]], [[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]], [[5.0, 6.0], [7.0, 8.0]]])
+T422_INC_1: Final = torch.tensor([[[1.0, 2.0], [3.0, 4.0]], [[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]], [[5.0, 6.0], [7.0, 8.0]]])
+# endregion: constants
 
 
 @pytest.mark.parametrize(
@@ -12,15 +39,13 @@ from src.siscos_nodes.util.tensor_common import (
             "expected_tensor",
         ],
         [
-            (torch.tensor([[0, 1], [2, 3]]), torch.tensor([[0.0, 0.3333], [0.6667, 1.0]])),
-            (torch.tensor([[1, 2], [3, 4]]), torch.tensor([[0.0, 0.3333], [0.6667, 1.0]])),
-            (torch.tensor([[5, 6], [7, 8]]), torch.tensor([[0.0, 0.3333], [0.6667, 1.0]])),
-            (torch.tensor([[[0, 1], [2, 3]], [[0, 1], [2, 3]]]), torch.tensor([[[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]]])),
-            (torch.tensor([[[0, 1], [2, 3]], [[1, 2], [3, 4]], [[5, 6], [7, 8]], [[5, 6], [7, 8]]]), torch.tensor([[[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]]])),
-            (torch.tensor([[[[0, 1], [2, 3]], [[1, 2], [3, 4]], [[5, 6], [7, 8]], [[5, 6], [7, 8]]],
-                           [[[0, 1], [2, 3]], [[1, 2], [3, 4]], [[5, 6], [7, 8]], [[5, 6], [7, 8]]]]), 
-                           torch.tensor([[[[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]]],
-                                         [[[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]], [[0.0, 0.3333], [0.6667, 1.0]]]])),
+            (T22_FILL_0, T22_FILL_0),
+            (T22_FILL_2, T22_FILL_1),
+            (T22_INC_0, T22_INC_NORM),
+            (T22_INC_1, T22_INC_NORM),
+            (T22_INC_5, T22_INC_NORM),
+            (T422_INC_0, T422_INC_NORM),
+            (T422_INC_1, T422_INC_NORM),
         ]
 )
 def test_normalize_tensor(tensor: torch.Tensor, expected_tensor: torch.Tensor):
@@ -32,6 +57,50 @@ def test_normalize_tensor(tensor: torch.Tensor, expected_tensor: torch.Tensor):
         f"Expected {expected_tensor}, but got {normalized_tensor}"
     )
 
+@pytest.mark.parametrize(
+        [
+            "threshold",
+            "tensor",
+            "expected_tensor",
+        ],
+        [
+            (0.0, T22_FILL_0, T22_FILL_0),
+            (0.0, T22_FILL_2, T22_FILL_1),
+            (0.0, T22_INC_0, T22_INC_NORM),
+            (0.0, T22_INC_1, T22_INC_NORM),
+            (0.0, T22_INC_5, T22_INC_NORM),
+            (0.0, T422_INC_0, T422_INC_NORM),
+            (0.0, T422_INC_1, T422_INC_NORM),
+            #
+            (0.5, T22_FILL_0, T22_FILL_0),
+            (0.5, T22_FILL_1, T22_FILL_1),
+            (0.5, T22_FILL_2, T22_FILL_1),
+            (0.5, T22_INC_0, T22_INC_NORM_2),
+            (0.5, T22_INC_1, T22_INC_NORM_2),
+            (0.5, T22_INC_5, T22_INC_NORM_2),
+            (0.5, T422_INC_0, T422_INC_NORM_2),
+            (0.5, T422_INC_1, T422_INC_NORM_2),
+            #
+            (1.0, T22_FILL_0, T22_FILL_0),
+            (1.0, T22_FILL_1, T22_FILL_0),
+            (1.0, T22_FILL_2, T22_FILL_0),
+            (1.0, T22_INC_0, T22_FILL_0),
+            (1.0, T22_INC_1, T22_FILL_0),
+            (1.0, T22_INC_5, T22_FILL_0),
+            (1.0, T422_INC_0, T422_FILL_0),
+            (1.0, T422_INC_1, T422_FILL_0),
+        ]
+)
+def test_threshold_normalize_tensor(threshold: float, tensor: torch.Tensor, expected_tensor: torch.Tensor):
+    # Normalize the tensor
+    normalized_tensor = threshold_and_normalize_tensor(tensor, threshold)
+
+    # Check if the normalized tensor is equal to the expected tensor
+    assert torch.allclose(normalized_tensor, expected_tensor, atol=1e-4), (
+        f"""Expected: {expected_tensor}
+            Received: {normalized_tensor}"""
+    )
+
 
 @pytest.mark.parametrize(
         [
@@ -39,7 +108,10 @@ def test_normalize_tensor(tensor: torch.Tensor, expected_tensor: torch.Tensor):
             "expected_tensor",
         ],
         [
-            (torch.tensor([[[0, 1], [2, 3]], [[5, 6], [7, 8]]]), torch.tensor([[[-1.0, -0.3333], [0.3333, 1.0]], [[-1.0, -0.3333], [0.3333, 1.0]]])),
+            (T22_INC_0.unsqueeze(0), T22_INC_NORM_NEG.unsqueeze(0)),
+            (T22_INC_1.unsqueeze(0), T22_INC_NORM_NEG.unsqueeze(0)),
+            (T422_INC_0, T422_INC_NORM_NEG),
+            (T422_INC_1, T422_INC_NORM_NEG),
         ]
 )
 def test_normalize_logits(tensor: torch.Tensor, expected_tensor: torch.Tensor):