b3a73e02af66670d5b9887a4a12c4a3cde7cd929
David Sisco <dsisco11@gmail.com>
2025-04-29 15:25:31 +0000

    feat: initial implementation of mask conversion node

--- src/siscos_nodes/masking/nodes/mask_conversion.py
+++ src/siscos_nodes/masking/nodes/mask_conversion.py
@@ -1,12 +1,15 @@
 
+from typing import Tuple, Union
+
 import torch
 from invokeai.app.invocations.baseinvocation import BaseInvocation, invocation
-from invokeai.app.invocations.fields import InputField
+from invokeai.app.invocations.fields import ImageField, InputField, TensorField
 from invokeai.app.services.shared.invocation_context import (
     ImageCategory,
     InvocationContext,
 )
 from torchvision.transforms.functional import to_pil_image as tensor_to_pil
+from torchvision.transforms.functional import to_tensor as pil_to_tensor
 
 from ...util.primitives import (
     EMaskingMode,
@@ -22,20 +25,45 @@ from ...util.tensor_common import apply_feathering_ellipse
     title="Convert Mask",
     tags=["mask", "convert"],
     category="mask",
-    version="0.0.1",
+    version="0.1.0",
 )
 class ConvertMaskInvocation(BaseInvocation):
     """Converts a gradient mask into a bit mask."""
 
-    mask: MaskingField = InputField(title="Mask")
+    mask: Union[MaskingField, TensorField, ImageField] = InputField(title="Mask")
     mode: LMaskingMode = InputField(title="Mode", default=EMaskingMode.GRADIENT)
     strength: float = InputField(title="Strength", default=0.25, description="Strength of the conversion.\nE.g: when converting TO a bool-mask, this is the threshold.\nWhen converting FROM a bool-mask, this is the feathering distance.")
 
+    def loadMaskTensor(self, context: InvocationContext) -> Tuple[EMaskingMode, torch.Tensor]:
+        """Resolve the mask tensor from the input field."""
+        if isinstance(self.mask, MaskingField):
+            return self.mask.mode, self.mask.load(context)
+        elif isinstance(self.mask, TensorField):
+            return EMaskingMode.BOOLEAN, context.tensors.load(self.mask.tensor_name)
+        elif isinstance(self.mask, ImageField):
+            return EMaskingMode.IMAGE_LUMINANCE, pil_to_tensor(context.images.get_pil(self.mask.image_name, mode='L'))
+        else:
+            raise ValueError(f"Unsupported mask type: {type(self.mask)}")
+        
+    def getMaskAssetId(self) -> str:
+        """Resolve the mask asset ID from the input field."""
+        if isinstance(self.mask, MaskingField):
+            return self.mask.asset_id
+        elif isinstance(self.mask, TensorField):
+            return self.mask.tensor_name
+        elif isinstance(self.mask, ImageField):
+            return self.mask.image_name
+        else:
+            raise ValueError(f"Unsupported mask type: {type(self.mask)}")
+
     def invoke(self, context: InvocationContext) -> MaskingNodeOutput:
-        tensor = self.mask.load(context)
-        if (self.mode == self.mask.mode): # converting to the same mask type (no-op)
+        tpl = self.loadMaskTensor(context)
+        originalMode:EMaskingMode = tpl[0]
+        tensor: torch.Tensor = tpl[1]
+
+        if (self.mode == originalMode): # converting to the same mask type (no-op)
             return MaskingNodeOutput(
-                mask=MaskingField(asset_id=self.mask.asset_id, mode=self.mask.mode)
+                mask=MaskingField(asset_id=self.getMaskAssetId(), mode=originalMode)
             )
 
         # Figure out if we are converting to or from a boolean mask so we can apply the strength correctly.
@@ -50,16 +78,16 @@ class ConvertMaskInvocation(BaseInvocation):
         match (self.mode):
             case EMaskingMode.IMAGE_ALPHA:
                 img = tensor_to_pil(tensor, mode='RGBA')
-                mask_out_id = context.images.save(img, image_category=ImageCategory.MASK)
+                mask_out_id = context.images.save(img, image_category=ImageCategory.MASK).image_name
             case EMaskingMode.IMAGE_COMPOUND:
                 img = tensor_to_pil(tensor, mode='RGBA')
-                mask_out_id = context.images.save(img, image_category=ImageCategory.MASK)
+                mask_out_id = context.images.save(img, image_category=ImageCategory.MASK).image_name
             case EMaskingMode.IMAGE_LUMINANCE:
                 img = tensor_to_pil(tensor, mode='L')
-                mask_out_id = context.images.save(img, image_category=ImageCategory.MASK)
+                mask_out_id = context.images.save(img, image_category=ImageCategory.MASK).image_name
             case EMaskingMode.BOOLEAN | EMaskingMode.GRADIENT:
                 mask_out_id = context.tensors.save(tensor)
 
         return MaskingNodeOutput(
-            mask=MaskingField(asset_id=mask_out_id, mode=self.mask.mode)
+            mask=MaskingField(asset_id=mask_out_id, mode=originalMode)
         )
