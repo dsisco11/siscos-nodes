c17b29ee91b22e8ba248eed91afc49bb51be47e4
David Sisco <dsisco11@gmail.com>
2025-05-03 12:50:25 +0000

    chore: add more slope-select test cases

--- tests/masking/test_slope_select.py
+++ tests/masking/test_slope_select.py
@@ -8,7 +8,7 @@ from siscos_nodes.src.siscos_nodes.masking.nodes import (
 )
 
 # region: constants
-# region 4x3 tensors
+# region mock tensors
 T43_1: Final = torch.tensor([
     [ 0.0, 0.2, -0.1,  0.5],
     [ 0.3, 0.8,  0.1, -0.2],
@@ -26,6 +26,24 @@ T43_3: Final = torch.tensor([
     [0.3, 0.8, 0.0, 0.5],
     [0.3, 0.0, 0.4, 0.6],
 ])
+
+T55_1: Final = torch.tensor([
+    [0.3, 0.3, 0.1, 0.3, 0.3],
+    [0.3, 0.0, 0.1, 0.0, 0.3],
+    [0.1, 0.1, 0.8, 0.1, 0.1],
+    [0.3, 0.0, 0.1, 0.0, 0.3],
+    [0.3, 0.3, 0.1, 0.3, 0.3],
+])
+
+T77_1: Final = torch.tensor([
+    [0.1, 0.1, 0.0, 0.0, 0.0, 0.1, 0.1],
+    [0.1, 0.0, 0.0, 0.1, 0.0, 0.0, 0.1],
+    [0.0, 0.0, 0.2, 0.4, 0.2, 0.0, 0.0],
+    [0.0, 0.1, 0.4, 0.8, 0.4, 0.1, 0.0],
+    [0.0, 0.0, 0.2, 0.4, 0.2, 0.0, 0.0],
+    [0.1, 0.0, 0.0, 0.1, 0.0, 0.0, 0.1],
+    [0.1, 0.1, 0.0, 0.0, 0.0, 0.1, 0.1],
+])
 # endregion
 # endregion: constants
 
@@ -33,6 +51,7 @@ T43_3: Final = torch.tensor([
 @pytest.mark.parametrize(
     "threshold, input, expected",
     [
+        #region: 4x3 tensors
         pytest.param(
             1.0,
             T43_1,
@@ -101,6 +120,39 @@ T43_3: Final = torch.tensor([
             ]).bool(),
             id="Test 8: Threshold 0.5",
         ),
+        #endregion
+
+        #region: 5x5 tensors
+        pytest.param(
+            0.8,
+            T55_1,
+            torch.tensor([
+                [0, 0, 1, 0, 0],
+                [0, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 0],
+                [0, 0, 1, 0, 0],
+            ]).bool(),
+            id="Test 9: Threshold 0.8",
+        ),
+        #endregion
+
+        #region: 7x7 tensors
+        pytest.param(
+            0.8,
+            T77_1,
+            torch.tensor([
+                [0, 0, 1, 1, 1, 0, 0],
+                [0, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 1, 1, 0],
+                [0, 0, 1, 1, 1, 0, 0],
+            ]).bool(),
+            id="Test 10: Threshold 0.8",
+        ),
+        #endregion
     ],
 )
 def test_slope_select(threshold: float, input: torch.Tensor, expected: torch.Tensor):
